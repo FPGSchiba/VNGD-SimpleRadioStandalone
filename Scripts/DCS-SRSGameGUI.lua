7e4522d09488d9aed9a88b97c1b1dd6493d9714a
Ciaran Fisher <cfisher88 [at] gmail [dot] com>
2016-07-05 21:13:40 +0100

    Fixed Radio Effects
    Fixed Radio effects
    Increased Window Transparency

---
+++ Scripts/DCS-SRSGameGUI.lua
@@ -0,0 +1,43 @@
+-- Version 1.0.5.0
+-- Make sure you COPY this file to the same location as the Export.lua as well.
+SRS = {}
+
+SRS.dbg = {}
+SRS.logFile = io.open(lfs.writedir()..[[Logs\DCS-SRS-GameGUI.log]], "w")
+function SRS.log(str)
+    if SRS.logFile then
+        SRS.logFile:write(str.."\n")
+        SRS.logFile:flush()
+    end
+end
+
+package.path  = package.path..";.\\LuaSocket\\?.lua"
+package.cpath = package.cpath..";.\\LuaSocket\\?.dll"
+
+local socket = require("socket")
+
+local JSON = loadfile("Scripts\\JSON.lua")()
+SRS.JSON = JSON
+
+SRS.UDPSendSocket = socket.udp()
+SRS.UDPSendSocket:settimeout(0)
+
+SRS.onPlayerChangeSlot = function(id)
+
+    local _update = {
+        name = "",
+        side = 0,
+    }
+
+    _update.name = net.get_player_info(id, "name" )
+	_update.side = net.get_player_info(id,'side')
+
+    SRS.log("Selected Slot  ID:"..id.." Name: ".._update.name.." Side: ".._update.side)
+
+    socket.try(SRS.UDPSendSocket:sendto(SRS.JSON:encode(_update).." \n", "239.255.50.10", 5068))
+end
+
+
+DCS.setUserCallbacks(SRS)
+
+net.log("Loaded - DCS-SRS GameGUI")
