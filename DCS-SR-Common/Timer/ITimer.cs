7fe489f51e16990c77c594bd4556123f9742de49
Ciaran <cfisher88@gmail.com>
2016-08-07 21:53:59 +0100

    Added Timer

---
+++ DCS-SR-Common/Timer/ITimer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Timer
+{
+    /// <summary>
+    /// Source: https://github.com/cabhishek/Time
+    /// </summary>
+    public interface ITimer
+    {
+        void Start();
+        void Stop();
+        void UpdateTimeInterval(TimeSpan interval);
+    }
+}
