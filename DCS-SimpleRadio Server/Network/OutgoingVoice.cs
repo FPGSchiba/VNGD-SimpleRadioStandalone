52c8e42caf7dd63a2d8b995899cfff51620dbf22
Ciaran <cfisher88@gmail.com>
2016-09-07 21:58:03 +0100

    First Pass
    First pass of trying to fix stutter.
    Server now has 3 threads - Incoming - Processing & outgoing

---
+++ DCS-SimpleRadio Server/Network/OutgoingVoice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Ciribob.DCS.SimpleRadio.Standalone.Common;
+
+namespace Ciribob.DCS.SimpleRadio.Standalone.Server.UI.Network
+{
+    public class OutgoingVoice
+    {
+      
+        public List<IPEndPoint> OutgoingEndPoints { get; set; }
+        public byte[] ReceivedPacket { get; set; }
+
+    }
+}
---
+++ DCS-SimpleRadio Server/Network/PendingPacket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciribob.DCS.SimpleRadio.Standalone.Server.UI.Network
+{
+    public class PendingPacket
+    {
+        public IPEndPoint ReceivedFrom { get; set; }
+        public byte[] RawBytes { get; set; }
+    }
+}
