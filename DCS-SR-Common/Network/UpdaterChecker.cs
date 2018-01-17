4e5e4c44243ad9e8b29f4627f3166da020355712
Ciaran Fisher <cfisher88 [at] gmail [dot] com>
2018-01-17 18:52:47 +0000

    Fix for mono headset
    SteelSeries headsets have a mono output for chat - this makes it work
    for them

--- DCS-SR-Common/Network/UpdaterChecker.cs
+++ DCS-SR-Common/Network/UpdaterChecker.cs
@@ -13,7 +13,7 @@ namespace Ciribob.DCS.SimpleRadio.Standalone.Common
     {
         public static readonly string MINIMUM_PROTOCOL_VERSION = "1.4.3.0";
 
-        public static readonly string VERSION = "1.4.5.0";
+        public static readonly string VERSION = "1.4.6.0";
 
         public static async void CheckForUpdate()
         {
--- Scripts/DCS-SimpleRadioStandalone.lua
+++ Scripts/DCS-SimpleRadioStandalone.lua
@@ -1,4 +1,4 @@
--- Version 1.4.5.0
+-- Version 1.4.6.0
 -- Special thanks to Cap. Zeen, Tarres and Splash for all the help
 -- with getting the radio information :)
 -- Add (without the --) To the END OF your Export.lua to enable Simple Radio Standalone :
@@ -1680,4 +1680,4 @@ function SR.nearlyEqual(a, b, diff)
     return math.abs(a - b) < diff
 end
 
-SR.log("Loaded SimpleRadio Standalone Export version: 1.4.5.0")
+SR.log("Loaded SimpleRadio Standalone Export version: 1.4.6.0")
