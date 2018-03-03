2dbf3d30cdc5c8307d877f524190341df1768c0c
Ciaran Fisher <cfisher88 [at] gmail [dot] com>
2018-03-03 15:29:38 +0000

    Version Bump

--- Scripts/DCS-SimpleRadioStandalone.lua
+++ Scripts/DCS-SimpleRadioStandalone.lua
@@ -1,4 +1,4 @@
--- Version 1.4.6.0
+-- Version 1.4.7.0
 -- Special thanks to Cap. Zeen, Tarres and Splash for all the help
 -- with getting the radio information :)
 -- Add (without the --) To the END OF your Export.lua to enable Simple Radio Standalone :
@@ -1700,4 +1700,4 @@ function SR.nearlyEqual(a, b, diff)
     return math.abs(a - b) < diff
 end
 
-SR.log("Loaded SimpleRadio Standalone Export version: 1.4.6.0")
+SR.log("Loaded SimpleRadio Standalone Export version: 1.4.7.0")
