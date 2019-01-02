ecd1b85617a1c3578569c820162b3ce6d214af24
Ciaran Fisher <cfisher88 [at] gmail [dot] com>
2019-01-02 17:17:25 +0000

    Version Bump

--- DCS-SR-Common/Network/UpdaterChecker.cs
+++ DCS-SR-Common/Network/UpdaterChecker.cs
@@ -19,7 +19,7 @@ namespace Ciribob.DCS.SimpleRadio.Standalone.Common
 
         public static readonly string MINIMUM_PROTOCOL_VERSION = "1.5.3.1";
 
-        public static readonly string VERSION = "1.5.6.0";
+        public static readonly string VERSION = "1.5.7.0";
 
         private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
