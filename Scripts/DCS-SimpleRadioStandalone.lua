55e53154e7b0dd5f810cd22c50c75d852e8f1745
Ciaran Fisher <cfisher88 [at] gmail [dot] com>
2019-02-01 19:31:47 +0000

    Added CEII
    Added CEII
    Fixed Overlay opacity

--- Scripts/DCS-SimpleRadioStandalone.lua
+++ Scripts/DCS-SimpleRadioStandalone.lua
@@ -121,6 +121,8 @@ LuaExportActivityNextEvent = function(tCurrent)
 
                  SR.lastKnownPos = _update.pos
 
+                 --SR.log(_update.unit.."\n\n")
+
 
                 if _update.unit == "UH-1H" then
                     _update = SR.exportRadioUH1H(_update)
@@ -162,6 +164,8 @@ LuaExportActivityNextEvent = function(tCurrent)
                     _update = SR.exportRadioC101CC(_update)
                 elseif _update.unit == "Hawk" then
                     _update = SR.exportRadioHawk(_update)
+                elseif _update.unit == "Christen Eagle II" then
+                    _update = SR.exportRadioEagleII(_update)
                 elseif _update.unit == "M-2000C" then
                     _update = SR.exportRadioM2000C(_update)
                 elseif _update.unit == "AV8BNA" then
@@ -948,6 +952,66 @@ function SR.exportRadioL39(_data)
     _data.control = 1; -- full radio - for expansion radios - DCS controls must be disabled
 
     _data.control = 1; -- full radio
+
+    return _data
+end
+
+function SR.exportRadioEagleII(_data)
+
+    _data.radios[1].name = "Intercom"
+    _data.radios[1].freq =100.0
+    _data.radios[1].modulation = 2 --Special intercom modulation
+    _data.radios[1].volume = 1--SR.getRadioVolume(0, 288,{0.0,0.8},false)
+
+    _data.radios[2].name = "KY-197A"
+    _data.radios[2].freq = SR.getRadioFrequency(5)
+    _data.radios[2].modulation = 0
+    _data.radios[2].volume =  SR.getRadioVolume(0, 364,{0.1,1.0},false)
+
+    if _data.radios[2].volume < 0 then
+        _data.radios[2].volume = 0
+    end
+
+
+    -- Intercom button depressed
+    -- if(SR.getButtonPosition(133) > 0.5 or SR.getButtonPosition(546) > 0.5) then
+    --     _data.selected = 0
+    --     _data.ptt = true
+    -- elseif (SR.getButtonPosition(134) > 0.5 or SR.getButtonPosition(547) > 0.5) then
+    --     _data.selected= 1
+    --     _data.ptt = true
+    -- else
+    --     _data.selected= 1
+    --      _data.ptt = false
+    -- end
+
+          -- Expansion Radio - Server Side Controlled
+    _data.radios[3].name = "AN/ARC-186(V)"
+    _data.radios[3].freq = 124.8*1000000 --116,00-151,975 MHz
+    _data.radios[3].modulation = 0
+    _data.radios[3].secFreq = 121.5*1000000
+    _data.radios[3].volume = 1.0
+    _data.radios[3].freqMin = 116*1000000
+    _data.radios[3].freqMax = 151.975*1000000
+    _data.radios[3].volMode = 1
+    _data.radios[3].freqMode = 1
+    _data.radios[3].expansion = true
+
+    -- Expansion Radio - Server Side Controlled
+    _data.radios[4].name = "AN/ARC-164 UHF"
+    _data.radios[4].freq = 251.0*1000000 --225-399.975 MHZ
+    _data.radios[4].modulation = 0
+    _data.radios[4].secFreq = 243.0*1000000
+    _data.radios[4].volume = 1.0
+    _data.radios[4].freqMin = 225*1000000
+    _data.radios[4].freqMax = 399.975*1000000
+    _data.radios[4].volMode = 1
+    _data.radios[4].freqMode = 1
+    _data.radios[4].expansion = true
+    _data.radios[4].encKey = 1
+    _data.radios[4].encMode = 1 -- FC3 Gui Toggle + Gui Enc key setting
+
+    _data.control = 0; -- HOTAS
 
     return _data
 end
