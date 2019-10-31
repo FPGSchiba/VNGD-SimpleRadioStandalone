4eff01a7b42852cc3584c878d405568eee511b78
Ciaran Fisher <cfisher88 [at] gmail [dot] com>
2019-10-31 21:43:25 +0000

    Version bump - 1.7.0.1

--- Scripts/DCS-SRS-AutoConnectGameGUI.lua
+++ Scripts/DCS-SRS-AutoConnectGameGUI.lua
@@ -1,4 +1,4 @@
--- Version 1.7.0.0
+-- Version 1.7.0.1
 -- ONLY COPY THIS WHOLE FILE IS YOU ARE GOING TO HOST A SERVER!
 -- The file must be in Saved Games\DCS\Scripts\Hooks or Saved Games\DCS.openalpha\Scripts\Hooks
 -- Make sure you enter the correct address into SERVER_SRS_HOST below.
@@ -210,4 +210,4 @@ SRSAuto.sendMessage = function(msg, showTime, gid)
 end
 
 DCS.setUserCallbacks(SRSAuto)
-net.log("Loaded - DCS-SRS-AutoConnect1.7.0.0")
+net.log("Loaded - DCS-SRS-AutoConnect1.7.0.1")
