0b146cf4fb0f1451d686adf4ce64e8277cdd5a3e
Ciaran Fisher <cfisher88 [at] gmail [dot] com>
2019-12-11 22:41:59 +0000

    Prepare 1.7.1.3 Release

--- Scripts/DCS-SRS/Scripts/DCS-SRS-OverlayGameGUI.lua
+++ Scripts/DCS-SRS/Scripts/DCS-SRS-OverlayGameGUI.lua
@@ -1,9 +1,9 @@
--- Version 1.7.1.2
+-- Version 1.7.1.3
 -- Make sure you COPY this file to the same location as the Export.lua as well! 
 -- Otherwise the Overlay will not work
 
 
-net.log("Loading - DCS-SRS Overlay GameGUI - Ciribob: 1.7.1.2 ")
+net.log("Loading - DCS-SRS Overlay GameGUI - Ciribob: 1.7.1.3 ")
 
 local base = _G
 
@@ -478,4 +478,4 @@ end
 
 DCS.setUserCallbacks(srsOverlay)
 
-net.log("Loaded - DCS-SRS Overlay GameGUI - Ciribob: 1.7.1.2 ")
+net.log("Loaded - DCS-SRS Overlay GameGUI - Ciribob: 1.7.1.3 ")
