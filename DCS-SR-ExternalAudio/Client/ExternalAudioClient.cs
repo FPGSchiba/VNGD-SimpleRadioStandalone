3daad6769abc7794fdb9cdd0accf0336d393c32c
Ciaran Fisher <cfisher88 [at] gmail [dot] com>
2020-08-15 14:04:56 +0100

    Self contained MP3 player for SRS

---
+++ DCS-SR-ExternalAudio/Client/ExternalAudioClient.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using Ciribob.DCS.SimpleRadio.Standalone.Common;
+using Ciribob.DCS.SimpleRadio.Standalone.Common.Network;
+using Ciribob.DCS.SimpleRadio.Standalone.ExternalAudioClient.Audio;
+using Ciribob.DCS.SimpleRadio.Standalone.ExternalAudioClient.Models;
+using Ciribob.DCS.SimpleRadio.Standalone.ExternalAudioClient.Network;
+using Easy.MessageHub;
+
+namespace Ciribob.DCS.SimpleRadio.Standalone.ExternalAudioClient
+{
+    internal class ExternalAudioClient
+    {
+        private string mp3Path;
+        private double freq;
+        private string modulation;
+        private int coalition;
+        private readonly int port;
+
+        private readonly string Guid = ShortGuid.NewGuid();
+
+        private bool _finished = false;
+        private DCSPlayerRadioInfo gameState;
+        private UdpVoiceHandler udpVoiceHandler;
+        private string name;
+
+        public ExternalAudioClient(string mp3Path, double freq, string modulation, int coalition, int port, string name)
+        {
+            this.mp3Path = mp3Path;
+            this.freq = freq;
+            this.modulation = modulation;
+            this.coalition = coalition;
+            this.port = port;
+            this.name = name;
+        }
+
+        public void Start()
+        {
+
+            MessageHub.Instance.Subscribe<ReadyMessage>(ReadyToSend);
+            MessageHub.Instance.Subscribe<DisconnectedMessage>(Disconnected);
+
+            gameState = new DCSPlayerRadioInfo();
+            gameState.radios[1].modulation = (RadioInformation.Modulation)(modulation == "AM" ? 0 : 1);
+            gameState.radios[1].freq = this.freq * 1000000; // get into Hz
+            gameState.radios[1].name = name;
+
+            var srsClientSyncHandler = new SRSClientSyncHandler(Guid, gameState,name, coalition);
+
+            srsClientSyncHandler.TryConnect(new IPEndPoint(IPAddress.Loopback, port));
+
+            while (!_finished)
+            {
+                Thread.Sleep(5000);
+            }
+            Console.WriteLine("Finished");
+
+            udpVoiceHandler?.RequestStop();
+            srsClientSyncHandler?.Disconnect();
+
+            MessageHub.Instance.ClearSubscriptions();
+        }
+
+        private void ReadyToSend(ReadyMessage ready)
+        {
+            if (udpVoiceHandler == null)
+            {
+                udpVoiceHandler = new UdpVoiceHandler(Guid, IPAddress.Loopback, port, gameState);
+                udpVoiceHandler.Start();
+                new Thread(SendAudio).Start();
+            }
+        }
+
+        private void Disconnected(DisconnectedMessage disconnected)
+        {
+            _finished = true;
+        }
+
+        private void SendAudio()
+        {
+            Console.WriteLine("Sending Audio");
+            MP3OpusReader mp3 = new MP3OpusReader(mp3Path);
+            foreach (var opusByte in mp3.GetOpusBytes())
+            {
+                //can use timer to run through it
+                Thread.Sleep(40);
+
+                if (!_finished)
+                {
+                    udpVoiceHandler.Send(opusByte, opusByte.Length);
+                }
+                else
+                {
+                    Console.WriteLine("Client Disconnected");
+                    return;
+                }
+            }
+
+            //get all the audio as Opus frames of 40 ms
+            //send on 40 ms timer 
+
+            //when empty - disconnect
+
+            Console.WriteLine("Finished Audio Buffer");
+            _finished = true;
+
+        }
+    }
+}
---
+++ DCS-SR-ExternalAudio/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciribob.DCS.SimpleRadio.Standalone.ExternalAudioClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 6)
+            {
+                Console.WriteLine("Error incorrect parameters - should be path frequency modulation coalition port name");
+                Console.WriteLine("Example: \"C:\\FULL\\PATH\\TO\\File.mp3\" 251.0 AM 1 5002 ciribob-robot");
+            }
+            else
+            {
+                string mp3 = args[0].Trim();
+                double freq = double.Parse(args[1].Trim(), CultureInfo.InvariantCulture);
+                string modulation = args[2].Trim().ToUpperInvariant();
+                int coalition = int.Parse(args[3].Trim());
+                int port = int.Parse(args[4].Trim());
+                string name = args[5].Trim();
+
+                ExternalAudioClient client = new ExternalAudioClient(mp3, freq, modulation, coalition, port, name);
+                client.Start();
+
+            }
+        }
+    }
+}
