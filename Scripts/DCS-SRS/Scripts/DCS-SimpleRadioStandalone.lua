05651cd42f0c6ff8d692b62e922a013f5797d6cc
Ciaran Fisher <cfisher88@gmail.com>
2021-06-16 17:24:23 +0100

    Release 1.9.7.0

--- Scripts/DCS-SRS/Scripts/DCS-SimpleRadioStandalone.lua
+++ Scripts/DCS-SRS/Scripts/DCS-SimpleRadioStandalone.lua
@@ -1,4 +1,4 @@
--- Version 1.9.6.0
+-- Version 1.9.7.0
 -- Special thanks to Cap. Zeen, Tarres and Splash for all the help
 -- with getting the radio information :)
 -- Run the installer to correctly install this file
@@ -3879,4 +3879,4 @@ function SR.tableShow(tbl, loc, indent, tableshow_tbls) --based on serialize_slm
     end
 end
 
-SR.log("Loaded SimpleRadio Standalone Export version: 1.9.6.0")
+SR.log("Loaded SimpleRadio Standalone Export version: 1.9.7.0")
