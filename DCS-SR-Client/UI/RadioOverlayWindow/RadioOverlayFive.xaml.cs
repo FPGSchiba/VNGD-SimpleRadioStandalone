36a52a2b99fd0cabdf0ea7fac9b280119c81c875
Archangel-93 <bjwilliams111012@gmail.com>
2023-10-08 22:42:59 -0400

    fixing mistake that caused a conflict

--- DCS-SR-Client/UI/RadioOverlayWindow/RadioOverlayFive.xaml.cs
+++ DCS-SR-Client/UI/RadioOverlayWindow/RadioOverlayFive.xaml.cs
@@ -127,37 +127,6 @@ namespace Ciribob.DCS.SimpleRadio.Standalone.Overlay
                     }
                 }
 
-                if (availableRadios == 6
-                    || dcsPlayerRadioInfo.radios.Length >= 6
-                    && dcsPlayerRadioInfo.radios[5].modulation != RadioInformation.Modulation.DISABLED)
-                {
-                    Radio5.Visibility = Visibility.Visible;
-                    Radio4.Visibility = Visibility.Visible;
-
-                    if (MinHeight != _originalMinHeight + (_radioHeight * 2))
-                    {
-                        MinHeight = (_originalMinHeight + (_radioHeight * 2));
-                        Recalculate();
-                    }
-                }
-                else if (availableRadios == 5
-                         || dcsPlayerRadioInfo.radios.Length >= 5
-                         && dcsPlayerRadioInfo.radios[4].modulation != RadioInformation.Modulation.DISABLED)
-                {
-                    Radio5.Visibility = Visibility.Collapsed;
-                    Radio4.Visibility = Visibility.Visible;
-                    if (MinHeight != _originalMinHeight + _radioHeight)
-                    {
-                        MinHeight = _originalMinHeight + _radioHeight;
-                        Recalculate();
-                    }
-                }
-                else
-                {
-                    ResetHeight();
-                }
-
-
                 if (availableRadios > 1)
                 {
                     if (dcsPlayerRadioInfo.control == DCSPlayerRadioInfo.RadioSwitchControls.HOTAS)
@@ -181,18 +150,6 @@ namespace Ciribob.DCS.SimpleRadio.Standalone.Overlay
             }
 
             FocusDCS();
-        }
-
-        private void ResetHeight()
-        {
-
-            Radio4.Visibility = Visibility.Collapsed;
-            Radio5.Visibility = Visibility.Collapsed;
-            if (MinHeight != _originalMinHeight)
-            {
-                MinHeight = _originalMinHeight;
-                Recalculate();
-            }
         }
 
         private void Recalculate()
