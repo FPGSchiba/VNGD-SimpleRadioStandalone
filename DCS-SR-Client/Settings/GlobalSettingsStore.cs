0f4f635df5ec9dcf19fb185a32b119cdbcf64d40
FPG Schiba <craftzockerlp@gmail.com>
2024-07-21 16:25:39 +0200

    Radio Enable saving
    * Implemented saving of Radio Enabled state over restarts and reconnects

--- DCS-SR-Client/Settings/GlobalSettingsStore.cs
+++ DCS-SR-Client/Settings/GlobalSettingsStore.cs
@@ -143,6 +143,17 @@ namespace Ciribob.DCS.SimpleRadio.Standalone.Client.Settings
         ClientY,
         #endregion
 
+        Radio1Enabled,
+        Radio2Enabled,
+        Radio3Enabled,
+        Radio4Enabled,
+        Radio5Enabled,
+        Radio6Enabled,
+        Radio7Enabled,
+        Radio8Enabled,
+        Radio9Enabled,
+        Radio10Enabled,
+
         MicAudioOutputDeviceId,
 
         CliendIdShort, // not used anymore
@@ -619,6 +630,18 @@ namespace Ciribob.DCS.SimpleRadio.Standalone.Client.Settings
             {GlobalSettingsKeys.ClientY.ToString(), "200"},
 
 #endregion
+
+            // Radio Toggle Settings
+            {GlobalSettingsKeys.Radio1Enabled.ToString(), "true"},
+            {GlobalSettingsKeys.Radio2Enabled.ToString(), "true"},
+            {GlobalSettingsKeys.Radio3Enabled.ToString(), "true"},
+            {GlobalSettingsKeys.Radio4Enabled.ToString(), "false"},
+            {GlobalSettingsKeys.Radio5Enabled.ToString(), "false"},
+            {GlobalSettingsKeys.Radio6Enabled.ToString(), "false"},
+            {GlobalSettingsKeys.Radio7Enabled.ToString(), "false"},
+            {GlobalSettingsKeys.Radio8Enabled.ToString(), "false"},
+            {GlobalSettingsKeys.Radio9Enabled.ToString(), "false"},
+            {GlobalSettingsKeys.Radio10Enabled.ToString(), "true"},
 
             //    {GlobalSettingsKeys.CliendIdShort.ToString(), ShortGuid.NewGuid().ToString()},
             {GlobalSettingsKeys.ClientIdLong.ToString(), Guid.NewGuid().ToString()},
