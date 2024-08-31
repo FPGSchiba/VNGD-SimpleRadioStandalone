a2a79593a4f6382ca856d713b663ed9a4594d4c7
Dabble <cameronforsberg@gmail.com>
2024-08-31 06:19:54 -0700

    added 5H menu button logic

--- DCS-SR-Client/UI/RadioOverlayWindow/RadioOverlayFiveHorizontal.xaml.cs
+++ DCS-SR-Client/UI/RadioOverlayWindow/RadioOverlayFiveHorizontal.xaml.cs
@@ -280,5 +280,11 @@ namespace Ciribob.DCS.SimpleRadio.Standalone.Overlay
                 }
             }
         }
+
+        private void ShowOverlayMenuSelect_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+            _toggleOverlay(true, 15);
+        }
     }
 }
