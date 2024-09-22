087825757b75af4ac0076ba0e08d06e245f88095
FPG Schiba <craftzockerlp@gmail.com>
2024-09-22 22:32:02 +0200

    Added Balance Control

---
+++ DCS-SR-Client/UI/Components/BalanceControl.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Xamarin.Forms;
+using IMultiValueConverter = System.Windows.Data.IMultiValueConverter;
+using IValueConverter = System.Windows.Data.IValueConverter;
+using Point = System.Windows.Point;
+
+namespace Ciribob.DCS.SimpleRadio.Standalone.Client.UI.Components
+{
+    public partial class BalanceControl : UserControl
+    {
+        private double _beginningRadius;
+        private bool _isPressed = false;
+        private Canvas _templateCanvas = null;
+        
+        public BalanceControl()
+        {
+            InitializeComponent();
+            _beginningRadius = this.Height / 2;
+        }
+
+        
+
+        private void Ellipse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //Enable moving mouse to change the value.
+            _isPressed = true;
+        }
+
+        private void Ellipse_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //Disable moving mouse to change the value.
+            _isPressed = false;
+        }
+
+        private void Ellipse_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isPressed)
+            {
+                //Find the parent canvas.
+                if (_templateCanvas == null)
+                {
+                    _templateCanvas = MyHelper.FindParent<Canvas>(e.Source as Ellipse);
+                    if (_templateCanvas == null) return;
+                }
+                //Canculate the current rotation angle and set the value.
+                const double RADIUS = 25;
+                Point newPos = e.GetPosition(_templateCanvas);
+                double angle = MyHelper.GetAngleR(newPos, RADIUS);
+                knob.Value = (knob.Maximum - knob.Minimum) * angle / (2 * Math.PI);
+            }
+        }
+    }
+
+    //The converter used to convert the value to the rotation angle.
+    public class ValueAngleConverter : IMultiValueConverter
+    {
+        #region IMultiValueConverter Members
+
+        public object Convert(object[] values, Type targetType, object parameter, 
+                      System.Globalization.CultureInfo culture)
+        {
+            double value = (double)values[0];
+            double minimum = (double)values[1];
+            double maximum = (double)values[2];
+
+            return MyHelper.GetAngle(value, maximum, minimum);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, 
+              System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+
+    //Convert the value to text.
+    public class ValueTextConverter : IValueConverter
+    {
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, 
+                  System.Globalization.CultureInfo culture)
+        {
+            double v = (double)value;
+            return String.Format("{0:F2}", v);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, 
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+
+    public static class MyHelper
+    {
+        //Get the parent of an item.
+        public static T FindParent<T>(FrameworkElement current)
+          where T : FrameworkElement
+        {
+            do
+            {
+                current = VisualTreeHelper.GetParent(current) as FrameworkElement;
+                if (current is T)
+                {
+                    return (T)current;
+                }
+            }
+            while (current != null);
+            return null;
+        }
+
+        //Get the rotation angle from the value
+        public static double GetAngle(double value, double maximum, double minimum)
+        {
+            double current = (value / (maximum - minimum)) * 360;
+            if (current == 360)
+                current = 359.999;
+
+            return current;
+        }
+
+        //Get the rotation angle from the position of the mouse
+        public static double GetAngleR(Point pos, double radius)
+        {
+            //Calculate out the distance(r) between the center and the position
+            Point center = new Point(radius, radius);
+            double xDiff = center.X - pos.X;
+            double yDiff = center.Y - pos.Y;
+            double r = Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
+
+            //Calculate the angle
+            double angle = Math.Acos((center.Y - pos.Y) / r);
+            Console.WriteLine("r:{0},y:{1},angle:{2}.", r, pos.Y, angle);
+            if (pos.X < radius)
+                angle = 2 * Math.PI - angle;            
+            if (Double.IsNaN(angle))
+                return 0.0;
+            else
+                return angle;
+        }
+    }
+}
