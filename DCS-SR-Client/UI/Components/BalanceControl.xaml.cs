fae5fce6ccede4e9b5e4dfec8d559044cbb66fb7
FPG Schiba <craftzockerlp@gmail.com>
2024-09-24 20:33:36 +0200

    Added Custom Size to Balancer
    * Added custom size via Radius (still a bit broken without Height and Width)
    * Added Right clamping to keep the values on one half
    * TODO: Added mouse wheel interaction
    * TODO: Fix the slider values ranging from -10 to 10 from left to right

--- DCS-SR-Client/UI/Components/BalanceControl.xaml.cs
+++ DCS-SR-Client/UI/Components/BalanceControl.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using NLog;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 using IMultiValueConverter = System.Windows.Data.IMultiValueConverter;
 using IValueConverter = System.Windows.Data.IValueConverter;
 using Point = System.Windows.Point;
@@ -13,14 +16,18 @@ namespace Ciribob.DCS.SimpleRadio.Standalone.Client.UI.Components
 {
     public partial class BalanceControl : UserControl
     {
-        private double _beginningRadius;
         private bool _isPressed = false;
         private Canvas _templateCanvas = null;
+        private Logger _logger = LogManager.GetCurrentClassLogger();
+        
+        // The value of the control.
+        public double Radius { get; set; }
         
         public BalanceControl()
         {
+            DataContext = this;
+            
             InitializeComponent();
-            _beginningRadius = this.Height / 2;
         }
 
         
@@ -39,20 +46,60 @@ namespace Ciribob.DCS.SimpleRadio.Standalone.Client.UI.Components
 
         private void Ellipse_MouseMove(object sender, MouseEventArgs e)
         {
-            if (_isPressed)
+            //Find the parent canvas.
+            if (_templateCanvas == null)
             {
-                //Find the parent canvas.
+                _templateCanvas = MyHelper.FindParent<Canvas>(e.Source as Ellipse);
                 if (_templateCanvas == null)
                 {
-                    _templateCanvas = MyHelper.FindParent<Canvas>(e.Source as Ellipse);
-                    if (_templateCanvas == null) return;
+                    _logger.Debug("Not finding the parent canvas.");
+                    return;
                 }
-                //Canculate the current rotation angle and set the value.
-                const double RADIUS = 25;
-                Point newPos = e.GetPosition(_templateCanvas);
-                double angle = MyHelper.GetAngleR(newPos, RADIUS);
+            }
+            //Canculate the current rotation angle and set the value.
+            
+            Point newPos = e.GetPosition(_templateCanvas);
+            double angle = GetAngleR(newPos);
+            
+            if (_isPressed)
+            {
                 knob.Value = (knob.Maximum - knob.Minimum) * angle / (2 * Math.PI);
             }
+        }
+        
+        public double GetAngleR(Point pos)
+        {
+            //Calculate out the distance(r) between the center and the position
+            Point center = new Point(Radius, Radius);
+            double xDiff = center.X - pos.X;
+            double yDiff = center.Y - pos.Y;
+            double r = Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
+
+            if (_isPressed && pos.Y - 5 > Radius)
+            {
+                _isPressed = false; 
+            }
+            if (pos.Y < Radius && Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                _isPressed = true;
+            }
+            
+            //Calculate the angle
+            double angle = Math.Acos((pos.Y > Radius ? center.Y - Radius : center.Y - pos.Y) / r);
+            _logger.Trace("r:{0}, x:{1} y:{2},angle:{3}.", r, pos.X, pos.Y, angle);
+            if (pos.X < Radius)
+                angle = 2 * Math.PI - angle;
+            if (Double.IsNaN(angle))
+                return 0.0;
+            else
+                return angle;
+        }
+
+        private void Ellipse_OnMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if(!_isPressed)
+                // TODO: Change this to look at the current angle and adjust the value based on that
+                knob.Value += e.Delta / 120;
         }
     }
 
@@ -102,6 +149,61 @@ namespace Ciribob.DCS.SimpleRadio.Standalone.Client.UI.Components
         #endregion
     }
 
+    public class SizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type  targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return (double)value * 2;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return (double)value / 2;
+            }
+            return null;
+        }
+    }
+
+    public class KnobConverter : IValueConverter
+    {
+        public object Convert(object value, Type  targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return MyHelper.GetKnobSizeFromRadius((double)value);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+    
+    public class OffsetConverter : IValueConverter
+    {
+        public object Convert(object value, Type  targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var knobSize = MyHelper.GetKnobSizeFromRadius((double)value);
+                return MyHelper.GetKnobOffset((double)value, knobSize);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
     public static class MyHelper
     {
         //Get the parent of an item.
@@ -123,6 +225,7 @@ namespace Ciribob.DCS.SimpleRadio.Standalone.Client.UI.Components
         //Get the rotation angle from the value
         public static double GetAngle(double value, double maximum, double minimum)
         {
+            // TODO: Change this method to clamp between 90 and 270 so we stay in the top half of the circle
             double current = (value / (maximum - minimum)) * 360;
             if (current == 360)
                 current = 359.999;
@@ -130,24 +233,15 @@ namespace Ciribob.DCS.SimpleRadio.Standalone.Client.UI.Components
             return current;
         }
 
-        //Get the rotation angle from the position of the mouse
-        public static double GetAngleR(Point pos, double radius)
+        public static double GetKnobSizeFromRadius(double radius)
         {
-            //Calculate out the distance(r) between the center and the position
-            Point center = new Point(radius, radius);
-            double xDiff = center.X - pos.X;
-            double yDiff = center.Y - pos.Y;
-            double r = Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
-
-            //Calculate the angle
-            double angle = Math.Acos((center.Y - pos.Y) / r);
-            Console.WriteLine("r:{0},y:{1},angle:{2}.", r, pos.Y, angle);
-            if (pos.X < radius)
-                angle = 2 * Math.PI - angle;            
-            if (Double.IsNaN(angle))
-                return 0.0;
-            else
-                return angle;
+            return radius / 4;
+        }
+        
+        public static double GetKnobOffset(double radius, double knobSize)
+        {
+            return radius - knobSize / 2;
         }
     }
 }
+
